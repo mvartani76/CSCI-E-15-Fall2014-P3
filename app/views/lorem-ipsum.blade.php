52236de05aff832b08fe1b482ac5d9edb7951a80
Mike Vartanian <mike.vartanian@gmail.com>
2014-10-13 00:57:25 -0400

    Initial add of lorem-ipsum text generator page that has a form input for selecting number of paragraphs. After form is submitted via POST, a route redirects back to page with numpars set.

---
+++ app/views/lorem-ipsum.blade.php
@@ -0,0 +1,30 @@
+<html>
+	<body>
+		@extends('master')
+		@section('header')
+			{{ HTML::style('css/bootstrap.min.css'); }}
+			@if(isset($breed))
+				{{link_to('/', 'Back to the overview')}}
+			@endif
+			<h2>This is my lorem-ipsum page</h2>
+		@stop
+		@section('content')
+			<div class="form-group">
+				{{ Form::open(array('url' => 'lorem-ipsum','method' =>'post')) }}
+				{{ Form::label('numpars', 'Number of Paragraphs')}}
+				{{ Form::text('numpars') }}
+				{{ Form::submit("Generate Text", array("class"=>"btn btn-default"))}}
+				{{ Form::close() }}
+			</div>
+			@if(isset($numpars))
+			<?php
+				$generator = new Badcow\LoremIpsum\Generator();
+				$paragraphs = $generator->getParagraphs($numpars);
+				echo implode('<p>', $paragraphs);
+			?>
+		</p>
+			@endif
+		@stop
+
+	</body>
+</html>
