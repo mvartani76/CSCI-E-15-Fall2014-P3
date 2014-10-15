db755c60a24fe406cfe94f23737304e96423d104
Mike Vartanian <mike.vartanian@gmail.com>
2014-10-15 01:18:07 -0400

    Added routes for the random user generator

--- app/routes.php
+++ app/routes.php
@@ -31,4 +31,28 @@ Route::post('/lorem-ipsum', function(){
         	->with('numpars', $numpars);
 });
 
+// Generate view when URI is /random-user
+Route::get('/random-user', function(){
+	return View::make('random-user');
+});
+
+// Generate a view when receiving post msg with 'numpars' variable
+// and then go back to the /lorem-ipsum view with 'numpars' set to
+// generate some lorem-ipsum text paragraphs
+Route::post('/random-user', function(){
+	$numusers = Input::get('numusers');
+	$address = Input::get('address');
+	$phonenum = Input::get('phonenum');
+	$incemail = Input::get('incemail');
+	$companyname = Input::get('companyname');
+	$birthdate = Input::get('birthdate');
+    return View::make('random-user')
+        	->with('numusers', $numusers)
+        	->with('address', $address)
+        	->with('phonenum', $phonenum)
+        	->with('incemail', $incemail)
+        	->with('companyname', $companyname)
+        	->with('birthdate', $birthdate);
+});
+
 ?>
